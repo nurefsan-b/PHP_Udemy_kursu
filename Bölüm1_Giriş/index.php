1e6e2d839d5263f13e14a93739f696ea65f92b6b
nurefşan <nrfsnbzkrt@gmail.com>
2025-05-16 13:49:14 +0300

    Birinci Bölüm:Giriş

---
+++ Bölüm1_Giriş/index.php
@@ -0,0 +1,12 @@
+<?php 
+echo"PHP eğitimine hoşgeldiniz,Bugün PHP'nin temelini öğreniyoruz.";
+echo "Nurefşan Bozkurt";
+echo "Udemy Eğitim Seti";
+//Bu bir tek satırlık yorumdur.
+# bu da tek satırlık yorumdur.
+
+/*
+çok satırlı yorum
+... 
+*/ 
+?>
